44fabbf368979f04466b7c46c9382727a21fb577
Anthony DeGennaro <adegennaro@bnl.gov>
2019-07-26 15:22:05 -0400

    Updates

--- image_structure/src/Voronoi.py
+++ image_structure/src/Voronoi.py
@@ -11,11 +11,38 @@ def voronoi( input_data , plot_metrics=False , output_dir='./' , output_name='' 
     
     xy0               = compute_voronoi_centers( input_data , filter_tolerance )
     vor               = Voronoi( xy0 )
-    vertices_internal , centers_internal , vol_internal = compute_nvertices_for_each_center(
+    vertices_internal , centers_internal , vol_internal , skewness_internal = compute_nvertices_for_each_center(
         xy0 , vor , [0,input_data.shape[0]] , [0,input_data.shape[1]] )
     dist_centers_to_vertices = compute_distance_center_to_vertices( centers_internal , vertices_internal )
 
-    return vertices_internal , centers_internal , vol_internal , dist_centers_to_vertices , vor
+    return vertices_internal , centers_internal , vol_internal , skewness_internal , dist_centers_to_vertices , vor
+
+def compute_cell_angles( vi ):
+    
+    p      = Polygon(vi)
+    theta  = np.zeros(len(vi))
+    coords = p.exterior.coords[0:-1]
+    
+    for i in range(len(vi)):
+        v1        = np.array( coords[i] )
+        v2        = np.array( coords[(i+1)%(len(coords))] )
+        v3        = np.array( coords[(i+2)%(len(coords))] )
+        a         = v1 - v2
+        b         = v3 - v2
+        theta[i]  = np.arccos( a.dot(b) / ( np.linalg.norm(a) * np.linalg.norm(b) ) )
+    
+    return theta
+
+def compute_skewness( vi ):
+    
+    theta     = compute_cell_angles( vi )
+    theta_0   = np.pi - 2*np.pi / len(vi)
+    theta_max = np.max( theta )
+    theta_min = np.min( theta )
+    skewness  = np.maximum( (theta_max - theta_0) / (np.pi - theta_0) , (theta_0 - theta_min) / theta_0 )
+    
+    return skewness
+
 
 def compute_distance_center_to_vertices( centers_internal , vertices_internal ):
 
@@ -63,6 +90,7 @@ def compute_nvertices_for_each_center( xy0 , vor , xminmax , yminmax):
     vertices_internal = []
     centers_internal  = []
     vol_internal      = []
+    skewness_internal = []
     
     for i in range(len( vor.regions )):
         vi  = np.array( [ vor.vertices[j] for j in vor.regions[i] ] )
@@ -75,10 +103,11 @@ def compute_nvertices_for_each_center( xy0 , vor , xminmax , yminmax):
                 vertices_internal.append( vi )
                 centers_internal.append( vor.points[np.where(vor.point_region == i)[0][0]] )
                 vol_internal.append( compute_volume(vi) )
+                skewness_internal.append( compute_skewness( vi ) )
         except:
             pass
     
-    return vertices_internal , centers_internal , vol_internal
+    return vertices_internal , centers_internal , vol_internal , skewness_internal
 
 
 def compute_volume( vi ):
